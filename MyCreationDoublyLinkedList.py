3286615e8d38b38445062b3388cfc99381bcb0f0
Neha Mishra <magicaneha@Nehas-MBP.invent.law>
2023-02-10 17:34:33 -0500

    More functions added

--- MyCreationDoublyLinkedList.py
+++ MyCreationDoublyLinkedList.py
@@ -90,16 +90,53 @@ class DoublyLinkedList:
                 itr.prev.next=itr.next
                 break
             itr=itr.next
-            count+=1
+            count+=1     
+            
+    def delete_this_data(self,data):
+        if self.head is None:
+            raise Exception ("List is empty")
+        if self.get_length()==1:
+            self.delete_at_beg()
+            return           
+        itr=self.head
+        while itr: 
+            if itr.next.data==data:
+                itr.next=itr.next.next
+                
+                return
+            itr=itr.next
+        raise Exception("Data doesnt exist")
 
+    def update_data(self,update_data,previous_data):
+        if self.head is None:
+            raise Exception ("List is empty")
+        if self.get_length()==1:
+            self.head.data=update_data
+            return
+        itr=self.head
+        while itr: 
+            if itr.data==previous_data:
+                itr.data=update_data
+                return
+            itr=itr.next
 
-
-        
-
-    
-
-
-
+    def update_at_index(self,index,data):
+        if self.head is None: 
+            raise Exception ("List is empty")
+        if index>self.get_length() or index<0:
+            raise Exception (index, "is invalid please enter upto",self.get_length)
+        if index==0:
+            self.head.data=data
+            return
+        itr=self.head
+        count=0
+        while itr:
+            
+            if count==index:
+                itr.data=data
+                return
+            itr=itr.next
+            count+=1  
         
     def get_length(self):
         count=0
@@ -125,6 +162,8 @@ ll.insert_at_beg(15)
 ll.insert_at_end(13)
 ll.insert_at_end(3)
 ll.insert_at_beg(25)
+ll.insert_at_end(36)
+ll.insert_at_beg(53)
 ll.insert_at_beg(35)
 ll.insert_at_end(43)
 ll.insert_at_end(31)
@@ -138,4 +177,11 @@ ll.print()
 ll.delete_at_end()
 ll.print()
 ll.delete_at_index(3)
+ll.print()
+ll.delete_this_data(15)
+ll.print()
+ll.update_data(34,13)
+
+ll.print()
+ll.update_at_index(3,37)
 ll.print()
