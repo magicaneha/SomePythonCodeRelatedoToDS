036d03cdd241920fa23aaca8124af36e943a39ab
Neha Mishra <magicaneha@Nehas-MacBook-Pro.local>
2023-02-10 21:45:25 -0500

    CircularAtEnd

--- MyCreationCircularLinkedList.py
+++ MyCreationCircularLinkedList.py
@@ -24,8 +24,22 @@ class circularLinkedList:
             node.next = node 
  
         self.head = node 
+    
+    def insert_at_end(self,data):
+        node=Node(data)
+        itr=self.head
+        if self.head is not None:
+            while itr.next is not self.head:
+                itr=itr.next
+            itr.next=node
+            node.next=self.head
+            
+            return
+        else:
+            self.insert_at_beg(data)    
 
-    def print_it(self):
+
+    def print(self):
         itr = self.head
         if self.head is not None:
             ll=' '
@@ -41,6 +55,7 @@ my_list = circularLinkedList()
 my_list.insert_at_beg (56)
 my_list.insert_at_beg (78)
 my_list.insert_at_beg (12)
+my_list.insert_at_end(23)
 
 
-my_list.print_it()
+my_list.print()
